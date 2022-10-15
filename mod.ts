7dbfc3c55221b2fba6de081cddb8d812b77e9709
ENvironmentSet <sbshw1@gmail.com>
2022-10-15 23:19:13 +0900

    implement next/prev button

--- mod.ts
+++ mod.ts
@@ -7,6 +7,7 @@ import { outdent } from 'https://deno.land/x/outdent@v0.8.0/src/index.ts';
 
 const port = parseInt(useEnvVar('PORT', 'Interaction endpoint port'))
 const PUB_KEY = useEnvVar('PUB_KEY', 'Application public key')
+const BOT_TOKEN = useEnvVar('BOT_TOKEN', 'Bot token')
 
 async function pingHandler(): Promise<Response> {
   return json({ type: 1 })
@@ -41,15 +42,81 @@ async function hoogleCommandHandler(jsonBody: any): Promise<Response> {
           }
         })
       ),
-      "components": [
+      components: [
         {
-          "type": 1,
-          "components": [
+          type: 1,
+          components: [
             {
-              "type": 2,
-              "label": "Click me!",
-              "style": 1,
-              "custom_id": "click_one"
+              type: 2,
+              label: 'previous',
+              style: 1,
+              custom_id: JSON.stringify({ type: 'prev', index: 0, query }),
+              disabled: true
+            },
+            {
+              type: 2,
+              label: 'next',
+              style: 1,
+              custom_id: JSON.stringify({ type: 'next', index: 0, query })
+            }
+          ]
+        }
+      ]
+    }
+  })
+}
+
+async function hoogleCommandActionHandler(jsonBody: any): Promise<Response> {
+  const { data: { custom_id: action }, message: { id, channel_id } } = jsonBody
+  const { type, index, query } = JSON.parse(action)
+  const nextIndex = type === 'prev' ? index - 1 : index + 1
+
+  const searchResult = await search(query, { start: nextIndex })
+
+  console.info(outdent`
+    search result: ${JSON.stringify(searchResult)}
+  `)
+
+  if (searchResult.length === 0) return json({ type: 4, data: { content: 'no search result' } })
+
+  await fetch(`https://discord.com/api/v10/channels/${channel_id}/messages/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': `Bot ${BOT_TOKEN}`
+    }
+  })
+
+  return json({
+    type: 4, // CHANNEL_MESSAGE_WITH_SOURCE
+    data: {
+      embeds: searchResult.map(
+        def => ({
+          title: def.item,
+          description: def.docs,
+          url: def.url,
+          color: 16750592, // 0xFF9800, yellow
+          author: {
+            name: `${def.package.name}/${def.module.name}`
+          }
+        })
+      ),
+      components: [
+        {
+          type: 1,
+          components: [
+            {
+              type: 2,
+              label: 'previous',
+              style: 1,
+              custom_id: JSON.stringify({ type: 'prev', index: nextIndex, query }),
+              disabled: nextIndex === 0
+            },
+            {
+              type: 2,
+              label: 'next',
+              style: 1,
+              custom_id: JSON.stringify({ type: 'next', index: nextIndex, query })
             }
           ]
 
@@ -91,6 +158,7 @@ const handler = async (request: Request): Promise<Response> => {
 
   if (request.method === 'POST' && jsonBody?.type === 1) return pingHandler()
   if (request.method === 'POST' && jsonBody?.type === 2 && jsonBody?.data?.name === 'hoogle') return hoogleCommandHandler(jsonBody)
+  if (request.method === 'POST' && jsonBody?.type == 3) return hoogleCommandActionHandler(jsonBody)
   else return new Response('cannot interpret request', { status: 404 })
 }
 
