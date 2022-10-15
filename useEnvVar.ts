f002d42bc2c85045a238c1f14523b387e02a3e89
ENvironmentSet <sbshw1@gmail.com>
2022-10-15 23:44:54 +0900

    use 'key' if 'usage' is not present

--- useEnvVar.ts
+++ useEnvVar.ts
@@ -1,8 +1,12 @@
-export function useEnvVar(key: string, usage: string = key): string {
+import { outdent } from 'https://deno.land/x/outdent@v0.8.0/src/index.ts';
+
+export function useEnvVar(key: string, usage?: string): string {
   const variable = Deno.env.get(key)
 
   if (variable === undefined)
-    throw new Error(`${usage} is not registered, please define environment variable '${key}'`)
+    throw new Error(outdent`
+      ${usage ?? `'${key}'`} is not registered, please define environment variable '${key}'
+    `)
 
   return variable
 }
